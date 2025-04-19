9e023ce043614effd077d3d1192bf5f0d0f83689
19010853 <khoinguyenminhk37@gmail.com>
2025-04-19 20:06:13 +0700

    add error and success meesage function

--- dashboard-frontend/src/store/Reducers/authReducer.js
+++ dashboard-frontend/src/store/Reducers/authReducer.js
@@ -1,15 +1,17 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
 
-export const admin_Login = createAsyncThunk(
-    'auth/admin_Login',
-    async(info) => {
+export const admin_login = createAsyncThunk(
+    'auth/admin_login',
+    async(info, {rejectWithValue, fulfillWithValue}) => {
         console.log(info)
         try {
             const {data} = await api.post('/admin-login', info, {withCredentials: true})
-            console.log(data)
+            // console.log(data)
+            return fulfillWithValue(data)
         } catch (error) {
-            console.log(error.response.data)
+            // console.log(error.response.data)
+            return rejectWithValue(error.response.data)
         }
     }
 )
@@ -23,11 +25,25 @@ export const authReducer = createSlice({
         userInformation: ''
     },
     reducers: {
-
+        messageClear: (state,_) => {
+            state.errorMessage = ''
+        }
     },
-    extraReducers: () => {
-
+    extraReducers: (builder) => {
+        builder
+        .addCase(admin_login.pending, (state, {payload}) => {
+            state.loader = true
+        })
+        .addCase(admin_login.rejected, (state, {payload}) => {
+            state.loader = false
+            state.errorMessage = payload.error 
+        })
+        .addCase(admin_login.fulfilled, (state, {payload}) => {
+            state.loader = false
+            state.successMessage = payload.message
+        })
     }
 })
 
 export default authReducer.reducer;
+export const {messageClear} = authReducer.actions
