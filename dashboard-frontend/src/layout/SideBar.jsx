d43b734da442c1ce5e3f4b302e325413f7ce8e59
19010853 <khoinguyenminhk37@gmail.com>
2025-04-20 13:46:27 +0700

    navigate design

--- dashboard-frontend/src/layout/SideBar.jsx
+++ dashboard-frontend/src/layout/SideBar.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BiLogOutCircle } from "react-icons/bi";
 
-const SideBar = () => {
+const Sidebar = () => {
   return (
     <div>
-      <h1>Side Bar</h1>
+      <ul>
+        <li>
+          <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
+            <span>
+              <BiLogOutCircle />
+            </span>
+            <span>Logout</span>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
 
-export default SideBar;
+export default Sidebar;
