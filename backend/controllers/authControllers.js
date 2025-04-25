cc4f1aa3821c9024fed9e97d426f53089778a927
19010853 <khoinguyenminhk37@gmail.com>
2025-04-25 23:33:50 +0700

    Seller sign in and log in edit

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,57 +1,140 @@
-const adminModel = require('../models/adminModel');
-const admin = require('../models/adminModel');
-const { responseReturn } = require('../utiles/response');
-const { createToken } = require('../utiles/tokenCreate');
-const bcrypt = require('bcrypt');
-
+const adminModel = require('../models/adminModel')
+const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
+const { responseReturn } = require('../utiles/response')
+const bcrpty = require('bcrypt')
+const { createToken } = require('../utiles/tokenCreate')
 
 class authControllers{
-    admin_login = async(req, res) => {
-        const {email, password} = req.body;
+   
+    admin_login = async(req,res) => {
+        const {email,password} = req.body
         try {
-            const admin = await adminModel.findOne({email}).select('+password');
-            // console.log(admin);
+            const admin = await adminModel.findOne({email}).select('+password')
+            // console.log(admin)
             if (admin) {
-               const match = await bcrypt.compare(password, admin.password);
-            //    console.log(match);
-            if (match) {
-                const token = await createToken({
-                    id : admin.id,
-                    role : admin.role,
-                })
-                res.cookie('accessToken', token, {
-                    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                })
-                responseReturn(res, 200, {token, message: "Login successful"});
+                const match = await bcrpty.compare(password, admin.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : admin.id,
+                        role : admin.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    }) 
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+
+
+
+                 
             } else {
-                responseReturn(res, 404, {error: "Wrong password"});
+                responseReturn(res,404,{error: "Email not Found"})
             }
-            } else {
-                responseReturn(res, 404, {error: "Email not found"});
-            }
+            
         } catch (error) {
-            responseReturn(res, 500, {error: error.message});
+            responseReturn(res,500,{error: error.message})
         }
-
-
+ 
     }
-    //End admin_login
+    // End Method 
 
-    getUser = async(req, res) => {
-        const {role, id} = req;
+
+    seller_login = async(req,res) => {
+        const {email,password} = req.body
         try {
-            if (role == 'admin') {
-                const user = await adminModel.findById(id)
-                responseReturn(res, 200, {userInfo : user});
+            const seller = await sellerModel.findOne({email}).select('+password')
+            // console.log(admin)
+            if (seller) {
+                const match = await bcrpty.compare(password, seller.password)
+                // console.log(match)
+                if (match) {
+                    const token = await createToken({
+                        id : seller.id,
+                        role : seller.role
+                    })
+                    res.cookie('accessToken',token,{
+                        expires : new Date(Date.now() + 7*24*60*60*1000 )
+                    }) 
+                    responseReturn(res,200,{token,message: "Login Success"})
+                } else {
+                    responseReturn(res,404,{error: "Password Wrong"})
+                }
+ 
+                 
             } else {
+                responseReturn(res,404,{error: "Email not Found"})
+            }
+            
+        } catch (error) {
+            responseReturn(res,500,{error: error.message})
+        }
+ 
+    }
+    // End Method 
+
+
+    seller_register = async(req, res) => {
+         const {email,name,password} = req.body
+         try {
+            const getUser = await sellerModel.findOne({email})
+            if (getUser) {
+                responseReturn(res,404,{error: 'Email Already Exit'})
+            }else{
+                const seller = await sellerModel.create({
+                    name,
+                    email,
+                    password: await bcrpty.hash(password, 10),
+                    method : 'menualy',
+                    shopInfo: {}
+                })
+               await sellerCustomerModel.create({
+                     myId: seller.id
+               })
+
+               const token = await createToken({ id : seller.id, role: seller.role })
+               res.cookie('accessToken',token, {
+                expires : new Date(Date.now() + 7*24*60*60*1000 )
+               })
+
+               responseReturn(res,201,{token,message: 'Register Success'})
+            }
+         } catch (error) {
+            responseReturn(res,500,{error: 'Internal Server Error'})
+         }
+    }
+    // End Method 
+
+
+
+
+
+
+
+    getUser = async (req, res) => {
+        const {id, role} = req;
+
+        try {
+            if (role === 'admin') {
+                const user = await adminModel.findById(id)
+                responseReturn(res, 200, {userInfo : user})
+            }else {
                 console.log('Seller Info')
             }
+            
         } catch (error) {
-            console.log(error.message);
+            console.log(error.message)
         }
-    }
-    //End getUser
+
+
+    } // End getUser Method 
+
+
+
 
 }
 
-module.exports = new authControllers();
+module.exports = new authControllers()
--- dashboard-frontend/src/views/auth/AdminLogin.jsx
+++ dashboard-frontend/src/views/auth/AdminLogin.jsx
@@ -4,6 +4,7 @@ import { admin_login, messageClear } from "../../store/Reducers/authReducer";
 import { PropagateLoader } from "react-spinners";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { overrideStyle } from "../../utils/utils";
 
 const AdminLogin = () => {
   const navigate = useNavigate();
@@ -27,15 +28,7 @@ const AdminLogin = () => {
   const submit = (e) => {
     e.preventDefault();
     dispatch(admin_login(state));
-    console.log(state);
-  };
-
-  const overrideStyle = {
-    display: "flex",
-    margin: "0 auto",
-    height: "24px",
-    justifyContent: "center",
-    alignItems: "center",
+    // console.log(state)
   };
 
   useEffect(() => {
@@ -48,7 +41,7 @@ const AdminLogin = () => {
       dispatch(messageClear());
       navigate("/");
     }
-  }, [errorMessage, successMessage, dispatch]);
+  }, [errorMessage, successMessage]);
 
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
@@ -98,7 +91,7 @@ const AdminLogin = () => {
               className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
             >
               {loader ? (
-                <PropagateLoader color="white" cssOverride={overrideStyle} />
+                <PropagateLoader color="#fff" cssOverride={overrideStyle} />
               ) : (
                 "Login"
               )}
