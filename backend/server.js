dc8bdd3ce5fb0bd1517725aed4eefa19f247bb0f
19010853 <khoinguyenminhk37@gmail.com>
2025-05-07 22:03:47 +0700

    add product show

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,22 @@ const cookieParser = require('cookie-parser')
 const { dbConnect } = require('./utiles/db')
 
 require('dotenv').config()
-  
+
 app.use(cors({
-    origin : ['http://localhost:3000'],
+    origin: ['http://localhost:3000'],
     credentials: true
 }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
 
-app.use('/api',require('./routes/authRoutes'))
-app.use('/api',require('./routes/dashboard/categoryRoutes'))
-app.use('/api',require('./routes/dashboard/productRoutes'))
-app.use('/api',require('./routes/dashboard/sellerRoutes'))
+app.use('/api', require('./routes/authRoutes'))
+app.use('/api', require('./routes/dashboard/categoryRoutes'))
+app.use('/api', require('./routes/dashboard/productRoutes'))
+app.use('/api', require('./routes/dashboard/sellerRoutes'))
+app.use('/api/home', require('./routes/home/homeRoutes'))
 
-app.get('/',(req,res) => res.send('Hello Server'))
+app.get('/', (req, res) => res.send('Hello Server'))
 const port = process.env.PORT
 dbConnect()
 app.listen(port, () => console.log(`Server is running on port ${port}`))
