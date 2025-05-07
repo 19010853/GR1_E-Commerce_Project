647cc48c26ba0bf4b3d02f5c7a6fc9a4e85609fb
19010853 <khoinguyenminhk37@gmail.com>
2025-05-07 22:04:16 +0700

    add product show in frontend

---
+++ frontend/src/store/reducers/homeReducer.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
+
+
+export const get_category = createAsyncThunk(
+    'product/get_category',
+    async (_, { fulfillWithValue }) => {
+        try {
+            const { data } = await api.get('/home/get-categorys')
+            // console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.respone)
+        }
+    }
+)
+// End Method 
+export const get_products = createAsyncThunk(
+    'product/get_products',
+    async (_, { fulfillWithValue }) => {
+        try {
+            const { data } = await api.get('/home/get-products')
+            console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.respone)
+        }
+    }
+)
+// End Method 
+
+
+
+
+export const homeReducer = createSlice({
+    name: 'home',
+    initialState: {
+        categorys: [],
+        products: [],
+        latest_product: [],
+        topRated_product: [],
+        discount_product: [],
+    },
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(get_category.fulfilled, (state, { payload }) => {
+                state.categorys = payload.categorys;
+            })
+            .addCase(get_products.fulfilled, (state, { payload }) => {
+                state.products = payload.products;
+                state.latest_product = payload.latest_product;
+                state.topRated_product = payload.topRated_product;
+                state.discount_product = payload.discount_product;
+            })
+
+    }
+})
+
+export default homeReducer.reducer
