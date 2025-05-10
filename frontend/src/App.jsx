6e5a5564e5829b777dabea466e65d08953a70e4d
19010853 <khoinguyenminhk37@gmail.com>
2025-05-10 10:59:40 +0700

    user-dashboard design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,13 @@ import { useDispatch} from 'react-redux';
 import CategoryShop from './pages/CategoryShop';
 import SearchProducts from './pages/SearchProducts';
 import Payment from './pages/Payment';
+import Dashboard from './pages/Dashboard';
+import ProtectUser from './utils/ProtectUser';
+import Index from './components/dashboard/Index';
+import Orders from './components/dashboard/Orders';
+import ChangePassword from './components/dashboard/ChangePassword';
+import Wishlist from './components/dashboard/Wishlist';
+import Chat from './components/dashboard/Chat';
 
 function App() {
   const dispatch = useDispatch()
@@ -30,11 +37,27 @@ function App() {
       <Route path='/register' element={<Register/>} />
       <Route path='/shops' element={<Shops/>} />
       <Route path='/card' element={<Card/>} />
-      <Route path='/payment' element={<Payment/>} />
       <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/payment' element={<Payment/>} />
       <Route path='/products?' element={<CategoryShop/>} />
       <Route path='/products/search?' element={<SearchProducts/>} />
       <Route path='/product/details/:slug' element={<Details/>} /> 
+
+      <Route path='/dashboard' element={<ProtectUser/>} >
+      <Route path='' element={<Dashboard/>} >        
+      <Route path='' element={<Index/>} />
+      <Route path='my-orders' element={<Orders/>} /> 
+      <Route path='change-password' element={<ChangePassword/>} /> 
+      <Route path='my-wishlist' element={<Wishlist/>} /> 
+      <Route path='chat' element={<Chat/>} /> 
+       
+       </Route> 
+      </Route>
+      
+       
+
+
+
 
     </Routes>
     
---
+++ frontend/src/components/Stripe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Stripe = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Stripe;
